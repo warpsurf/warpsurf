91a8d02d6097a1246c7fbee15c80a8e26b193944
jonathanroberts <jdr53@cam.ac.uk>
2026-03-02 16:31:23 +0000

    Add roles barrel export and remove original legacy files superseded by .deprecated.ts copies

---
+++ apps/background/src/workflows/multiagent/roles/index.ts
@@ -0,0 +1,7 @@
+export { Commodore } from './commodore';
+export { Quartermaster } from './quartermaster';
+export { Captain } from './captain';
+export type { CaptainConfig } from './captain';
+export { Sailor } from './sailor';
+export type { SailorResult } from './sailor';
+export { SailorPrompt } from './sailor-prompt';
